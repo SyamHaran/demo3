78d95789d6c415232405ba9ea568a88c7a2dafbc
SyamHaran. <syamharans@gmail.com>
2024-11-10 23:51:45 +0530

    home

--- myproject3_app/src/App.jsx
+++ myproject3_app/src/App.jsx
@@ -1,12 +1,24 @@
-import logo from './logo.svg';
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home';
 
 function App() {
   return (
-    <div className="App">
-      <Counter />
-    </div>
+    <Router>
+      <div>
+       
+          
+        <Routes>
+          <Route path="/" element={<Home />} />
+          
+        </Routes>
+      </div>
+    </Router>
   );
 }
+
+
 
 export default App;
